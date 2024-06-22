0a60b6a43a226a5864bed664e63fd91f210b351b
flyingfish2205 <panther6025@gmail.com>
2024-06-22 09:55:08 -0400

    first commit

---
+++ flyingfish/eleventy.config.js
@@ -0,0 +1,26 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+    return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(
+      format || "dd LLLL yyyy",
+    );
+  });
+
+  eleventyConfig.addPassthroughCopy("src\\assets\\css");
+  eleventyConfig.addPassthroughCopy("src\\assets\\js");
+  eleventyConfig.addPassthroughCopy("src\\assets\\img");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "dist",
+    },
+  };
+};
